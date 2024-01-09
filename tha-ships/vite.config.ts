c0fd85c9960827976ae60651a27967d0b32b4084
SILMARIL\DanielBogdan <danieel.bogdan@gmail.com>
2024-01-09 13:39:04 +0200

    - setup libraries + ts

--- tha-ships/vite.config.ts
+++ tha-ships/vite.config.ts
@@ -1,14 +1,57 @@
-import { fileURLToPath, URL } from 'node:url';
+import { AliasOptions, defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'url';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue(), basicSsl()],
     resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
+        alias: getAliases()
+    },
+    server: {
+        host: true,
+        https: true
     }
 });
+
+function getAliases(): AliasOptions {
+    return [
+        {
+            find: '~bootstrap',
+            replacement: fileURLToPath(new URL('./node_modules/@types/bootstrap', import.meta.url))
+        },
+        {
+            find: '@/src',
+            replacement: fileURLToPath(new URL('./src', import.meta.url))
+        },
+        {
+            find: '@/views',
+            replacement: fileURLToPath(new URL('./src/views', import.meta.url))
+        },
+        {
+            find: '@/components',
+            replacement: fileURLToPath(new URL('./src/components', import.meta.url))
+        },
+        {
+            find: '@/types',
+            replacement: fileURLToPath(new URL('./src/types', import.meta.url))
+        },
+        {
+            find: '@/stores',
+            replacement: fileURLToPath(new URL('./src/stores', import.meta.url))
+        },
+        {
+            find: '@/api',
+            replacement: fileURLToPath(new URL('./src/api', import.meta.url))
+        },
+        {
+            find: '@/router',
+            replacement: fileURLToPath(new URL('./src/router', import.meta.url))
+        },
+        {
+            find: '@/assets',
+            replacement: fileURLToPath(new URL('./src/assets', import.meta.url))
+        }
+    ];
+}
