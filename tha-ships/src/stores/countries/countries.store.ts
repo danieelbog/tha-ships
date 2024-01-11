befade44a2564f30a43450837002ea28144d76e2
SILMARIL\DanielBogdan <danieel.bogdan@gmail.com>
2024-01-11 16:17:42 +0200

    - add comma seperated search, equal number not working

--- tha-ships/src/stores/countries/countries.store.ts
+++ tha-ships/src/stores/countries/countries.store.ts
@@ -6,42 +6,64 @@ import { IFilterApply } from '@/src/types/IFilter';
 import { getProperty } from '@/src/utils/property-traversal';
 
 export const useCountriesStore = defineStore('countries', () => {
-    const initCountries = ref([] as ICountryInfo[]);
+    const initCountries = ref<ICountryInfo[]>([]);
 
     const getCountries = async (filterApplyDto?: IFilterApply): Promise<ICountryInfo[]> => {
         try {
-            if (initCountries.value.length > 0) return initCountries.value;
-
-            const response = await api.get('https://restcountries.com/v3.1/all');
-            initCountries.value = response.data;
+            if (!initCountries.value || initCountries.value.length == 0) {
+                const response = await api.get('https://restcountries.com/v3.1/all');
+                initCountries.value = response.data;
+            }
 
             if (
                 !filterApplyDto?.selectedProperty ||
                 !filterApplyDto?.selectedFilter ||
-                filterApplyDto?.searchValue === undefined
+                !filterApplyDto?.searchValue
             ) {
                 return initCountries.value;
             }
+
+            const searchValues: string[] = filterApplyDto.searchValue
+                .toString()
+                .split(',')
+                .map((value) => value.trim());
 
             return initCountries.value.filter((countryInfo: ICountryInfo) => {
                 const propertyValue = getProperty(countryInfo, filterApplyDto.selectedProperty);
                 switch (filterApplyDto.selectedFilter) {
                     case 'eq':
-                        return propertyValue === filterApplyDto.searchValue;
+                        return searchValues.some((searchVal) => {
+                            console.log('propertyValue', typeof propertyValue);
+                            // console.log('searchVal', searchVal);
+                            console.log('eq', propertyValue === searchVal);
+
+                            if (typeof searchVal === 'number')
+                                return propertyValue === Number(searchVal);
+
+                            return propertyValue === searchVal;
+                        });
                     case 'ne':
-                        return propertyValue !== filterApplyDto.searchValue;
+                        return searchValues.every((searchVal) => propertyValue !== searchVal);
                     case 'gt':
-                        return propertyValue > filterApplyDto.searchValue;
+                        return searchValues.every((searchVal) => propertyValue > searchVal);
                     case 'lt':
-                        return propertyValue < filterApplyDto.searchValue;
+                        return searchValues.every((searchVal) => propertyValue < searchVal);
                     case 'substringOf':
-                        return String(propertyValue).includes(String(filterApplyDto.searchValue));
+                        return searchValues.some((searchVal) =>
+                            String(propertyValue).includes(String(searchVal))
+                        );
                     case 'notSubstringOf':
-                        return !String(propertyValue).includes(String(filterApplyDto.searchValue));
+                        return searchValues.every(
+                            (searchVal) => !String(propertyValue).includes(String(searchVal))
+                        );
                     case 'startswith':
-                        return String(propertyValue).startsWith(String(filterApplyDto.searchValue));
+                        return searchValues.some((searchVal) =>
+                            String(propertyValue).startsWith(String(searchVal))
+                        );
                     case 'endswith':
-                        return String(propertyValue).endsWith(String(filterApplyDto.searchValue));
+                        return searchValues.some((searchVal) =>
+                            String(propertyValue).endsWith(String(searchVal))
+                        );
                     default:
                         return false;
                 }
